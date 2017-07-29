826d59f10b33730e10cbc5d6110e62b5f822db78
Sajeras <sageras8888@gmail.com>
2017-07-30 00:52:04 +0200

    FIestaaaaa

---
+++ Assets/[Game Assets]/Scripts/EnemyController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+   // Use this for initialization
+
+
+   DamageController _damageController;
+   public GameObject explosion;
+   public GameObject _Muerte;
+   public GameObject visual;
+   SpriteRenderer _visual;
+   public void Awake()
+   {
+      this._damageController = GetComponent<DamageController>();
+      this._damageController.OnDead = this.OnDead;
+     _visual  = GetComponent<SpriteRenderer>();
+
+
+   }
+
+       
+	private void explosion_()
+   {
+         explosion.SetActive(true);
+
+   }
+
+  void OnDead()
+   {
+
+
+      GetComponent<BoxCollider2D>().enabled = false;
+      Animator anim = _Muerte.GetComponent<Animator>();
+
+      anim.enabled = false;
+      
+      _visual.enabled = !_visual.enabled;
+      explosion_();
+     
+      Invoke("destruir",1);
+
+   }
+
+   void destruir()
+   {
+
+      Destroy(_Muerte);
+      
+
+   }
+
+}
